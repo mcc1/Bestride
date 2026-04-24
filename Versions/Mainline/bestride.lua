31af92c3104f1d8b63f15a3d47aaf9b52902c1ee
mcc <mcc@mccma.cc>
2026-04-24 22:30:53 +0800

    update bestride

--- Versions/Mainline/bestride.lua
+++ Versions/Mainline/bestride.lua
@@ -26,28 +26,67 @@ function BeStride:GetMountInfoBySpellID(id)
 end
 
 function BeStride:IsSpellUsable(spell)
+    local isSecretValue = issecretvalue
+    local canAccessValue = canaccessvalue
+
     if C_Spell.IsSpellUsable then
-        return C_Spell.IsSpellUsable(spell)
+        local usable = C_Spell.IsSpellUsable(spell)
+        if isSecretValue and isSecretValue(usable) and (not canAccessValue or not canAccessValue(usable)) then
+            return false
+        end
+        return usable
     elseif C_Spell.IsUsableSpell then
-        return C_Spell.IsUsableSpell(spell)
+        local usable = C_Spell.IsUsableSpell(spell)
+        if isSecretValue and isSecretValue(usable) and (not canAccessValue or not canAccessValue(usable)) then
+            return false
+        end
+        return usable
     elseif IsUsableSpell then
-        return IsUsableSpell(spell)
+        local usable = IsUsableSpell(spell)
+        if isSecretValue and isSecretValue(usable) and (not canAccessValue or not canAccessValue(usable)) then
+            return false
+        end
+        return usable
     else
         return false
     end
 end
 
 function BeStride:GetSpellInfo(spell)
+	local isSecretValue = issecretvalue
+	local canAccessValue = canaccessvalue
+
 	if C_Spell.GetSpellInfo then
 		local info = C_Spell.GetSpellInfo(spell)
+		if isSecretValue and isSecretValue(info) and (not canAccessValue or not canAccessValue(info)) then
+			return nil
+		end
 		if info and type(info) == "table" then
+			if isSecretValue and isSecretValue(info.name) and (not canAccessValue or not canAccessValue(info.name)) then
+				return nil
+			end
+			if isSecretValue and isSecretValue(info.spellID) and (not canAccessValue or not canAccessValue(info.spellID)) then
+				return nil
+			end
 			return { name = info.name, spellID = info.spellID }
 		else
 			local name,_,_,_,_,_,spellID = C_Spell.GetSpellInfo(spell)
+			if isSecretValue and isSecretValue(name) and (not canAccessValue or not canAccessValue(name)) then
+				return nil
+			end
+			if isSecretValue and isSecretValue(spellID) and (not canAccessValue or not canAccessValue(spellID)) then
+				return nil
+			end
 			return { name = name, spellID = spellID }
 		end
 	elseif GetSpellInfo then
 		local name,_,_,_,_,_,spellID = GetSpellInfo(spell)
+		if isSecretValue and isSecretValue(name) and (not canAccessValue or not canAccessValue(name)) then
+			return nil
+		end
+		if isSecretValue and isSecretValue(spellID) and (not canAccessValue or not canAccessValue(spellID)) then
+			return nil
+		end
 		return { name = name, spellID = spellID }
 	else
 		return nil
@@ -55,16 +94,36 @@ function BeStride:GetSpellInfo(spell)
 end
 
 function BeStride:GetSpellOnCooldown(spell)
+    local isSecretValue = issecretvalue
+    local canAccessValue = canaccessvalue
+    local canAccessTable = canaccesstable
+
     if C_Spell.GetSpellCooldown then
 		local info = C_Spell.GetSpellCooldown(spell)
+		if isSecretValue and isSecretValue(info) and (not canAccessValue or not canAccessValue(info)) then
+			return true
+		end
+		if canAccessTable and not canAccessTable(info) then
+			return true
+		end
 		if info and type(info) == "table" then
-			return info.duration ~= 0
+			local duration = info.duration
+			if isSecretValue and isSecretValue(duration) and (not canAccessValue or not canAccessValue(duration)) then
+				return true
+			end
+			return duration ~= 0
 		else
 			local onCooldown, _, _, _ = GetSpellCooldown(195072)
+			if isSecretValue and isSecretValue(onCooldown) and (not canAccessValue or not canAccessValue(onCooldown)) then
+				return true
+			end
 			return onCooldown ~= 0
 		end
 	elseif GetSpellCooldown then
 		local onCooldown, _, _, _ = GetSpellCooldown(195072)
+		if isSecretValue and isSecretValue(onCooldown) and (not canAccessValue or not canAccessValue(onCooldown)) then
+			return true
+		end
 		return onCooldown ~= 0
 	else
 		return nil
